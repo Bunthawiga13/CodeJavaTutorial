cfed3f9d9fba642c68e1ba7493fd4b2a40dcd889
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-07-25 14:35:09 +0700

    Create TestNum.java

---
+++ TestNum.java
@@ -0,0 +1,13 @@
+public class TestNum {
+    public static void main(String[] args) {
+        int x, y; //ประกาศตัวแปรจำนวนเต็ม
+        x = 10;
+        y = x+1;
+        System.out.println("x = " +x+ " y = " +y);
+        y = y++ ;
+        System.out.println("x = " +x+ " y = " +y);
+        y = ++y ;
+        System.out.println("x = " +x+ " y = " +y);
+    }
+    
+}
