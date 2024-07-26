7253875c6f1eb9aaa81dd5e0d455f3ff4e040b0a
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-07-26 14:52:20 +0700

    Create ifAct.java

---
+++ ifAct.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ifAct {
+    public static void main(String[] args) {
+        final int Poin = 60;
+        int inputpoin ;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your poin : ");
+        inputpoin = scan.nextInt();
+        System.out.println("You entered : " +inputpoin);
+        if(inputpoin > 60)
+        System.out.println("you are pass");
+        if(inputpoin < 60)
+        System.out.println("you are not pass");
+    }
+}
