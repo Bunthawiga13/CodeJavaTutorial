fdd3b6ff48624075cb701b24131c0d12172a946d
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-07-26 14:53:34 +0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class test {
+    public static void main(String[] args) {
+        double x = 2.5;
+        double y = -1.5;
+        int m = 18;
+        int n = 4;
+        double g = Math.sqrt(Math.sqrt(n));
+        System.out.println(g);
+    }
+}
