74409b0cc372a7471dcf96874ef2199bf75ee941
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-07-26 13:34:54 +0700

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,14 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String message = "Java is Great Fun";
+        String upper = message.toLowerCase();
+        String Lower = message.toLowerCase();
+        int SringSize = message.length();
+        char letter = message.charAt(3);
+
+        System.out.println(upper);
+        System.out.println(Lower);
+        System.out.println(SringSize);
+        System.out.println(letter);
+    }
+}
