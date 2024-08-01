8452f7cb25bfca3ff53a51b45df0e6e78fe7b3fe
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-08-01 10:28:07 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++){
+            System.out.println("result in forLoop : "+i);
+        }
+
+        int iwhile = 1;
+        while (iwhile <= 5) {
+            System.out.println("Hello");
+            iwhile++ ;
+            
+        }
+    }
+}
