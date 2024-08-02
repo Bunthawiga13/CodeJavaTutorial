07b44969db4704d362e9b42ed4d7ad755216ad2c
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-08-02 10:10:22 +0700

    Create Callname.java

---
+++ Callname.java
@@ -0,0 +1,11 @@
+public class Callname {
+    public static void main(String[] args) {
+        System.out.println("My name");
+        for(int i = 1; i <= 10; i++){
+            bunthawiga();
+        }
+    }
+    public static void bunthawiga(){ 
+        System.out.println("EMMY BUNTHAWIGA");
+    }
+}
