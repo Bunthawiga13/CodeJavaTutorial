9abfa2579e0d6abd30dae94d16dde85825b59287
Bunthawiga13 <152472689+Bunthawiga13@users.noreply.github.com>
2024-08-09 13:31:48 +0700

    Update student.java

--- student.java
+++ student.java
@@ -1,23 +1,21 @@
 public class student {
-    private String id;
     private String name;
-    private double gpa;
-    public void setDetails(String ID, String n, double GPA){
-        id = ID;
-        name = n;
-        gpa = GPA;
+    private String email;
+
+    public student(){
+        name = "Unassined";
+        email =  "Unassined";
     }
-    public void showDetails(){
-        System.out.println("ID : "+id);
-        System.out.println("NAME : "+name);
-        System.out.println("GPA : "+gpa);
+    public void setName(String studentName){
+        name = studentName;
     }
-    public static void main(String[] args) {
-        student sc1 = new student();
-        sc1.setDetails("12123", "Buntha", 3.50);
-        sc1.showDetails();
-        student sc2 = new student();
-        sc2.setDetails("32321", "wipa", 3.33);
-        sc2.showDetails();
+    public void setEmail(String addess){
+        email = addess;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
     }
 }
